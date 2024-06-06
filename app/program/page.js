a84fabb2398db762c5a0ac3d676fa9d32650b235
Kilencedik <smg.c.osztaly@gmail.com>
2024-06-06 20:01:23 +0300

    tabor

--- app/program/page.js
+++ app/program/page.js
@@ -1,6 +1,6 @@
 export default function Program() {
     return(
-        <div className="flex flex-col w-screen h-[30rem] justify-center items-center pt-10">
+        <div className="flex flex-col w-screen h-full justify-center items-center pt-10">
             <div className="w-4/5 sm:w-3/4 md:w-2/3 lg:w-1/2 h-full border-4 rounded-2xl border-sky-500 bg-sky-400">
                 <div className="p-4">
                     <p>&emsp;Az alábbiakban a tábor részletes programja található meg. Az utolsó napon a szülőket is várjuk
