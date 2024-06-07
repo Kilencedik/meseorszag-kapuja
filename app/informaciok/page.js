42b25242bf33e01cb2ee97a704d5ca9bacd24b99
Kilencedik <smg.c.osztaly@gmail.com>
2024-06-07 17:34:19 +0300

    tabor

---
+++ app/informaciok/page.js
@@ -0,0 +1,28 @@
+export default function Informaciok() {
+    return(
+        <div className="flex justify-center items-center w-full h-fit mt-10">
+                <div className="w-4/5 h-fit border-4 border-sky-600 flex flex-col items-center justify-center rounded-xl">
+                        <div className="text-stone-700 p-14">
+                            Tisztelt szülők!
+                            <br/><br/>
+                            A Segítő Mária Római Katolikus Gimnázium 9. C osztályában tanuló diákok mentortanáraik segítségével
+                            nappali mesetábort tartanak <b>2024. július 29-31.</b> között, "Meseország kapuja" címmel. A táborba szeretettel várunk
+                            minden mesék iránt érdeklődő <b>1-4</b> osztályos gyereket.
+                            A tábor helyszíne: a Segítő Mária Római Katolikus Gimnázium bentlakása (utcaszám stb.)
+                            A tábor ára <b>90 lej</b>, ami a napi ebéd költségét valamint egy-két kézműves kellék árát fedezi.
+                            Szükséges dokumentumok: <b>szülői beleegyezés</b>, <b>orvosi igazolás</b>, <b>adatvédelmi beleegyezés</b>.
+                            A program <b>8-16</b> óra között zajlik, egy ebéd és egy tízórai szünettel. A tízórait, vizet a gyerek hozza
+                            otthonról. Amennyiben a résztevő gyermek bármilyen ételallergiával, érzékenységgel rendelkezik, a speciális
+                            étrend biztosítására sajnos nincs lehetőségünk. A részletes program az alábbi linken található meg:
+                            https://meseorszag-kapuja.vercel.app/program
+                            A tábor a gyerekek előadásával zárul, amelyben egy, a tábor során megismert mesét fognak a szülőknek
+                            előadni. Erre a tábor utolsó napján, <b>2024. július 31-én</b> kerül sor, <b>16</b> órai kezdettel.
+                            Jelentkezési határidő: <b>2024. július 18.</b>
+                            A helyek száma korlátozott.
+                            <br/><br/>
+                            Érdeklődni a <b>0757 530 645</b> telefonszámon lehet, hétköznapokon <b>16-18</b> között.
+                        </div>
+                </div>
+        </div>
+    )
+}
--- app/layout.js
+++ app/layout.js
@@ -24,11 +24,12 @@ export default async function RootLayout({ children }) {
 
 export function Header() {
     return (
-        <div className="w-screen h-44 flex flex-col">
+        <div className="w-screen min-h-44 grow flex flex-col">
             <div className="bg-sky-500 flex items-center p-14 h-28 text-4xl font-bold">Meseország Kapuja</div>
-            <div className="flex bg-sky-600 w-screen h-16 justify-around items-center text-xl font-bold">
-                <Link className="bg-sky-500 p-1.5 rounded px-3 sm:px-10 hover:opacity-80" href="/program">Program</Link>
-                <Link className="bg-sky-500 p-1.5 rounded px-3 sm:px-10 hover:opacity-80" href="/jelentkezes">Jelentkezés</Link>
+            <div className="flex bg-sky-600 w-screen min-h-16 justify-around items-center text-xl font-bold flex-wrap grow">
+                <Link className="bg-sky-500 p-1.5 rounded px-3 sm:px-10 hover:opacity-80 my-2" href="/program">Program</Link>
+                <Link className="bg-sky-500 p-1.5 rounded px-3 sm:px-10 hover:opacity-80 my-2" href="/jelentkezes">Jelentkezés</Link>
+                <Link className="bg-sky-500 p-1.5 rounded px-3 sm:px-10 hover:opacity-80 my-2" href="/informaciok">Információk</Link>
             </div>
         </div>
         )
