4b8aa2e46645baa3f615180daf882f9c2a69efd5
Kilencedik <smg.c.osztaly@gmail.com>
2024-06-07 17:42:46 +0300

    tabor

--- app/informaciok/page.js
+++ app/informaciok/page.js
@@ -6,14 +6,14 @@ export default function Informaciok() {
                             Tisztelt szülők!
                             <br/><br/>
                             A Segítő Mária Római Katolikus Gimnázium 9. C osztályában tanuló diákok mentortanáraik segítségével
-                            nappali mesetábort tartanak <b>2024. július 29-31.</b> között, "Meseország kapuja" címmel. A táborba szeretettel várunk
+                            nappali mesetábort tartanak <b>2024. július 29-31.</b> között, <q>Meseország kapuja</q> címmel. A táborba szeretettel várunk
                             minden mesék iránt érdeklődő <b>1-4</b> osztályos gyereket.
                             A tábor helyszíne: a Segítő Mária Római Katolikus Gimnázium bentlakása (utcaszám stb.)
                             A tábor ára <b>90 lej</b>, ami a napi ebéd költségét valamint egy-két kézműves kellék árát fedezi.
                             Szükséges dokumentumok: <b>szülői beleegyezés</b>, <b>orvosi igazolás</b>, <b>adatvédelmi beleegyezés</b>.
                             A program <b>8-16</b> óra között zajlik, egy ebéd és egy tízórai szünettel. A tízórait, vizet a gyerek hozza
                             otthonról. Amennyiben a résztevő gyermek bármilyen ételallergiával, érzékenységgel rendelkezik, a speciális
-                            étrend biztosítására sajnos nincs lehetőségünk. A részletes program a "Program" fül alatt található meg.
+                            étrend biztosítására sajnos nincs lehetőségünk. A részletes program a <q>Program</q> fül alatt található meg.
                             A tábor a gyerekek előadásával zárul, amelyben egy, a tábor során megismert mesét fognak a szülőknek
                             előadni. Erre a tábor utolsó napján, <b>2024. július 31-én</b> kerül sor, <b>16</b> órai kezdettel.
                             Jelentkezési határidő: <b>2024. július 18.</b>
