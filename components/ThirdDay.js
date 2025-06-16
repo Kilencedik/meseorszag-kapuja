d549e46e9192038b3ca97a3603faf0c8b5452efa
Kilencedik <smg.c.osztaly@gmail.com>
2025-06-16 12:09:27 +0300

    Update ThirdDay.js

--- components/ThirdDay.js
+++ components/ThirdDay.js
@@ -1,18 +1,1 @@
-export default function ThirdDay() {
-    return(
-        <div className="pt-5 px-5"><text className="text-xl">Szerda - Július 31.</text>
-            <ul className="font-normal pt-2 pb-5">
-                <li>8.00 - érkezés</li>
-                <li>8.00-9.00 - ébresztő játékok</li>
-                <li>9.00-9.30 - tízórai szünet</li>
-                <li>9.30-11.00 - előadás előkészítése</li>
-                <li>11.00-12.00 - első próba</li>
-                <li>12.00-12.30 - szünet</li>
-                <li>12.30-14.00 - próba, játékok</li>
-                <li>14.00-15.00 - ebéd</li>
-                <li>15.00-15.30 - második próba</li>
-                <li>15.30-17.00 - előadás, zárás</li>
-            </ul>
-        </div>
-    )
-}
+export default function ThirdDay() { return( <div className="pt-5 px-5"><text className="text-xl">Szerda - Július 16.</text> <ul className="font-normal pt-2 pb-5"> <li>8.00 - érkezés</li> <li>8.00-9.00 - bábok készítése</li> <li>9.00-9.30 - tízórai szünet</li> <li>9.30-11.00 - bábok készítése</li> <li>11.00-12.00 – játék, szünet</li> <li>12.00-12.30 - szünet</li> <li>12.30-14.00 – bábok, forgatókönyv készítése</li> <li>14.00-15.00 - ebéd</li> <li>15.00-16.00 - levezető játékok</li> </ul> </div> )}
