7fb894c717a739c60933d59dec5296e633bcf7d3
Kilencedik <smg.c.osztaly@gmail.com>
2025-06-16 12:05:50 +0300

    Update SecondDay.js

--- components/SecondDay.js
+++ components/SecondDay.js
@@ -1,16 +1,1 @@
-export default function SecondDay() {
-    return(
-        <div className="pt-5 px-5"><text className="text-xl">Kedd - Július 30.</text>
-            <ul className="font-normal pt-2 pb-5">
-                <li>8.00 - érkezés</li>
-                <li>8.00-9.00 - bábok, forgatókönyv, díszlet</li>
-                <li>9.00-9.30 - tízórai szünet</li>
-                <li>9.30-11.00 - bábok, forgatókönyv, díszlet</li>
-                <li>11.00-12.00 - játék, szünet</li>
-                <li>12.00-14.00 - bábok, forgatókönyv, díszlet</li>
-                <li>14.00-15.00 - ebéd</li>
-                <li>15.00-16.00 - levezető játékok</li>
-            </ul>
-        </div>
-    )
-}
+export default function ThirdDay() { return( <div className="pt-5 px-5"><text className="text-xl">Szerda - Július 16.</text> <ul className="font-normal pt-2 pb-5"> <li>8.00 - érkezés</li> <li>8.00-9.00 - bábok készítése</li> <li>9.00-9.30 - tízórai szünet</li> <li>9.30-11.00 - bábok készítése</li> <li>11.00-12.00 – játék, szünet</li> <li>12.00-12.30 - szünet</li> <li>12.30-14.00 – bábok, forgatókönyv készítése</li> <li>14.00-15.00 - ebéd</li> <li>15.00-16.00 - levezető játékok</li> </ul> </div> )}
